82c9bc3d6a003074afd9f14f056233f2a23c3541
plh34 <37965183+plh34@users.noreply.github.com>
2019-07-16 07:11:04 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import firebase from 'firebase';
 import ReduxThunk from 'redux-thunk';
+import { FIREBASE_API_KEY,
+         FIREBASE_AUTH_DOMAIN,
+         FIREBASE_DB_URL,
+         FIREBASE_PROJECT_ID,
+         FIREBASE_STORAGE_BUCKET,
+         FIREBASE_SENDER_ID,
+         FIREBASE_APP_ID
+       } from 'react-native-dotenv';
 import reducers from './reducers';
 import LoginForm from './components/LoginForm';
 import Router from './Router';
@@ -10,13 +18,13 @@ import Router from './Router';
 class App extends Component {
   componentWillMount() {
     const firebaseConfig = {
-    apiKey: 'AIzaSyDZKwXXg581aUhyu3B7xftZJfHA_caxxow',
-    authDomain: 'manager-58d1b.firebaseapp.com',
-    databaseURL: 'https://manager-58d1b.firebaseio.com',
-    projectId: 'manager-58d1b',
-    storageBucket: 'manager-58d1b.appspot.com',
-    messagingSenderId: '662234912137',
-    appId: '1:662234912137:web:dd9705112c49ee32'
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,,
+    databaseURL: FIREBASE_DB_URL,,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_SENDER_ID,
+    appId: FIREBASE_APP_ID
   };
   // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
